f019ec316f54bbcb85b70a520ee5a550bae9039d
alejandro20019 <aferiag@unal.edu.co>
2023-08-06 19:01:34 -0500

    produccion

--- django_crud_auth/settings.py
+++ django_crud_auth/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jzmzb=$)j(&jd1aj_5h02)1kj(it72@ide(x6)pp5@z$*9$#+c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'djangotodo.azurewebsites.net']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.whitenoisemiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +127,6 @@ LOGIN_URL = '/sigin/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.compressedmanifeststaticfilesstorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
