17c9bf8d24b2203c6bcc22f9f9b87024edac1ab5
alejandro20019 <aferiag@unal.edu.co>
2023-08-09 12:37:51 -0500

    si

--- django_crud_auth/settings.py
+++ django_crud_auth/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost:5432/mysite',
+            default='postgresql://postgres:postgres@localhost/postgres',
             conn_max_age=600
         )
     }
